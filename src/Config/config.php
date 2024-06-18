7051e2cbfec8ca5afc5cf94e52e981fe55459145
pietroconti89 <pietroconti89@gmail.com>
2024-06-18 12:08:43 +0200

    Update config.php

--- src/Config/config.php
+++ src/Config/config.php
@@ -20,6 +20,16 @@ return [
         // See https://docs.guzzlephp.org/en/stable/request-options.html
         'guzzle' => [],
     ],
+    'fcmv1' => [
+        'priority' => 'normal',
+        'dry_run' => false,
+        'projectId' => 'my-project-id',
+        'jsonFile' => __DIR__ . '/fcmCertificates/file.json',
+        // 'concurrentRequests' => 5, // Optional, default 10
+        // Optional: Default Guzzle request options for each FCM request
+        // See https://docs.guzzlephp.org/en/stable/request-options.html
+        'guzzle' => [],
+    ],
     'apn' => [
         'certificate' => __DIR__ . '/iosCertificates/apns-dev-cert.pem',
         'passPhrase' => 'secret', //Optional
